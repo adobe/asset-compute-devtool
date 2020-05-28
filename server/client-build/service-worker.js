c58b61f33c3cf61259b79ba321a6bc515ea3ce08
dhendric <dhendric@adobe.com>
2020-05-27 18:16:30 -0600

    debug

--- server/client-build/service-worker.js
+++ server/client-build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.693843c2352bf26a82993a2da9971cfa.js"
+  "/precache-manifest.2a7cd79d1d396b1511a18ef0f6bd37ac.js"
 );
 
 self.addEventListener('message', (event) => {
