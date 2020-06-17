5e7404a1a1c758e2b993e21ab79a5553e9bfa923
jmatthewc11 <32098169+jmatthewc11@users.noreply.github.com>
2020-06-17 16:47:24 -0400

    Delete build.bat

--- client/build.bat
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-IF EXIST "../server/client-build" (
-    rmdir /S /Q "../server/client-build"
-)
-mkdir "../server/client-build"
-move build/static ../server/client-build
-move build\\* ../server/client-build
